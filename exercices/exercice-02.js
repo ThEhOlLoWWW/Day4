9c1dd4f85e0f1cbfa80a9747fa5bde709d599061
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-11 11:29:08 +0100

    ex2 done

--- exercices/exercice-02.js
+++ exercices/exercice-02.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const tableau = [];
+
+ tableau.unshift("Yassine");
+  tableau.unshift("Sara");
+   tableau.unshift("Amine");
+
+console.log(tableau);
