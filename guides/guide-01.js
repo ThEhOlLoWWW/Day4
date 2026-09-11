684b89b4163ee643f2171c3cc4582b1de0a4e5e5
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-11 10:16:07 +0100

    guide 1 finished

--- guides/guide-01.js
+++ guides/guide-01.js
@@ -9,4 +9,11 @@
  */
 'use strict';
 
-// TODO: écris ta solution ici.
+const fruits = ["Pomme", "Banane", "Orange"];
+
+fruits[1] = "Mangue";
+
+console.log(fruits[1]);
+console.log(fruits[fruits.length - 1]);
+
+
